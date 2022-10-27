53f424a833ea6df111c327d22d5ca86c3e9d7b37
Scot Bontrager <cloudkucooland@gmail.com>
2022-10-27 03:01:53 +0000

    add

---
+++ src/model/association.ts
@@ -0,0 +1,22 @@
+export default class Association {
+  id: string;
+  name: string;
+  weight: number;
+  season: string;
+  proper: number;
+  week: number;
+  prayer: string;
+  reference: any;
+
+  constructor(obj: any) {
+    this.id = obj.id;
+    const d = obj.data();
+
+    this.name = d.Name;
+    this.season = d.Season;
+    this.week = d.Week;
+    this.proper = d.Proper;
+    this.weight = d.Weight;
+    this.reference = d.Reference;
+  }
+}
