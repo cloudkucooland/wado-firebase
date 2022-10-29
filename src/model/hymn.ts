a3f5bfb02482cf23fd0686c48d7084075f72e8c1
Scot Bontrager <cloudkucooland@gmail.com>
2022-10-29 04:31:31 +0000

    lots of work

--- src/model/hymn.ts
+++ src/model/hymn.ts
@@ -5,11 +5,15 @@ export default class hymn {
   license: boolean;
   lastEditor: string;
   lastEdited: string;
+  hymntune: string;
+  hymnmeter: string;
 
   constructor(obj: any) {
     this.name = obj.Name;
     this.body = obj.Body;
     this.reviewed = obj.Reviewed ? obj.Reviewed : false;
     this.license = obj.License ? obj.License : false;
+    this.hymntune = obj.Hymntune ? obj.Hymntune : false;
+    this.hymnmeter = obj.Hymnmeter ? obj.Hymnmeter : false;
   }
 }
