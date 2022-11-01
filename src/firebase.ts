292db288970611cd78116d888ad3af0682969bd1
Scot Bontrager <cloudkucooland@gmail.com>
2022-11-01 02:04:09 +0000

    enable auth

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
-// import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 import { notifyInfo, notifyWarn } from "./notify";
 // import { Hymn, Lection, Prayer, Psalm } from "./model";
@@ -18,7 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// const auth = getAuth(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 enableIndexedDbPersistence(db).catch((err) => {
