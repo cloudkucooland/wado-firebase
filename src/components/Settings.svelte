ccf3688b8d760dfd20e314255f9141a7494f66c4
Scot Bontrager <cloudkucooland@gmail.com>
2022-11-02 00:30:20 +0000

    draft 1

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,15 +1,53 @@
 <script>
-  import { FlatToast, ToastContainer } from "svelte-toasts";
+  import { debugOn, showMedia } from "../model/preferences";
+  import { auth } from "../firebase";
+  import { collection, query, where, getDocs } from "firebase/firestore";
+  import { onMount } from "svelte";
+
+  import {
+    Container,
+    Col,
+    Row,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+  } from "sveltestrap";
+
+  onMount((e) => {
+    console.log("Setttings onMount", e); 
+  });
+
 </script>
 
-<main class="cover-container text-center mx-auto">
-  <ToastContainer let:data>
-    <FlatToast {data} />
-  </ToastContainer>
-  <h1 class="display-2 font-weight-bold">WADO</h1>
-  <p class="h2">Settings</p>
-  <hr />
-  <ol>
-    <li>settings</li>
-  </ol>
-</main>
+<Container>
+  <Row>
+    <Col>
+      <Card class="mb-2">
+        <CardHeader>Settings</CardHeader>
+          <CardBody>
+            <Form>
+              <FormGroup>
+                <Label for="debugOn">Debug</Label>
+                <Input name="debugOn" id="debugOn" type="checkbox" value="true" on:change={(e) => { debugOn.set(e.srcElement.value); }} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="showMedia">Show Media Links</Label>
+                <Input
+                  name="showMedia"
+                  id="showMedia"
+                  type="checkbox"
+                  value="true"
+                  on:change={(e) => { showMedia.set(e.srcElement.value); }}
+                />
+              </FormGroup>
+            </Form>
+          </CardBody>
+      </Card>
+    </Col>
+  </Row>
+</Container>
