7ec17baef591b7c8a32285f0f9a890f641b79b16
Scot Bontrager <cloudkucooland@gmail.com>
2022-11-02 02:22:26 +0000

    fill in

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { debugOn, showMedia } from "../model/preferences";
+  import { debugOn, showMedia, showEdit } from "../model/preferences";
   import { auth } from "../firebase";
   import { collection, query, where, getDocs } from "firebase/firestore";
   import { onMount } from "svelte";
@@ -19,9 +19,12 @@
   } from "sveltestrap";
 
   onMount((e) => {
-    console.log("Setttings onMount", e); 
+    // console.log("Setttings onMount", e);
   });
 
+  $: debugChecked = $debugOn ? "checked" : "";
+  $: editChecked = $showEdit ? "checked" : "";
+  $: mediaChecked = $showMedia ? "checked" : "";
 </script>
 
 <Container>
@@ -29,24 +32,46 @@
     <Col>
       <Card class="mb-2">
         <CardHeader>Settings</CardHeader>
-          <CardBody>
-            <Form>
-              <FormGroup>
-                <Label for="debugOn">Debug</Label>
-                <Input name="debugOn" id="debugOn" type="checkbox" value="true" on:change={(e) => { debugOn.set(e.srcElement.value); }} />
-              </FormGroup>
-              <FormGroup>
-                <Label for="showMedia">Show Media Links</Label>
-                <Input
-                  name="showMedia"
-                  id="showMedia"
-                  type="checkbox"
-                  value="true"
-                  on:change={(e) => { showMedia.set(e.srcElement.value); }}
-                />
-              </FormGroup>
-            </Form>
-          </CardBody>
+        <CardBody>
+          <Form>
+            <FormGroup>
+              <Label for="debugOn">Debug</Label>
+              <Input
+                name="debugOn"
+                id="debugOn"
+                type="checkbox"
+                {debugChecked}
+                on:change={(e) => {
+                  debugOn.set(e.srcElement.checked);
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="showMedia">Show Media Links</Label>
+              <Input
+                name="showMedia"
+                id="showMedia"
+                type="checkbox"
+                {mediaChecked}
+                on:change={(e) => {
+                  showMedia.set(e.srcElement.checked);
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="showEdit">Show Edit Links</Label>
+              <Input
+                name="showEdit"
+                id="showEdit"
+                type="checkbox"
+                {editChecked}
+                on:change={(e) => {
+                  showEdit.set(e.srcElement.checked);
+                }}
+              />
+            </FormGroup>
+          </Form>
+        </CardBody>
       </Card>
     </Col>
   </Row>
