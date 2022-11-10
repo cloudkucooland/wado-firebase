0a4cda024dec8877dd4fb7d7df8f36739d8267a5
Scot Bontrager <cloudkucooland@gmail.com>
2022-11-10 05:00:56 +0000

    add admin screen

---
+++ src/components/Admin.svelte
@@ -0,0 +1,119 @@
+<script>
+  import { Container, Col, Row } from "sveltestrap";
+</script>
+
+<Container>
+  <Row>
+    <Col>
+      <h3>Prayer Management</h3>
+      <a href="#/prayers/prayer">Prayer List</a>
+
+      <h3>Browse</h3>
+      <a href="#/browse">Browse by location</a>
+      <div>
+        Use this to see which prayers are connected at various locations
+      </div>
+
+      <h3>Lectionary</h3>
+      <a href="#/lectionary">Lectionary List</a>
+      <div>Not written yet</div>
+
+      <h3>Settings</h3>
+      <a href="#/settings">Settings</a>
+      <div>User settings</div>
+    </Col>
+  </Row>
+
+  <Row>
+    <Col>
+      <h2>WADO Overview</h2>
+      <div>
+        <em>The top level of WADO are the offices.</em> These are hard-coded
+        into the system, but can be viewed on
+        <a
+          href="https://github.com/cloudkucooland/wado-firebase/tree/main/src/components"
+          >GitHub</a
+        >. Offices contain some fixed elements, such as rubrics, and many
+        dynamic elements that change from seasons-to-season, week-to-week, and
+        day-to-day.
+      </div>
+      <div>
+        <em>The dynamic elements are locations</em>. A location consists of
+        <em>associations</em>
+        to <em>prayers</em>.
+      </div>
+      <div>
+        A location can be unique or used in multiple offices. Each location has
+        a name. By convention, location names are defined in
+        ALL-CAPITOL-LETTERS. Locations that are used in a specific office start
+        with that office name, (e.g. LAUDS-CONFESSION). Locations that are used
+        in multiple offices do not (e.g. FORGIVEN)
+      </div>
+      <div>
+        An association consists of the location, the prayer, and data about when
+        to display that prayer.
+      </div>
+      <div>
+        If a <em>calendar date</em> (mm-dd month-day, year is ignored) is set,
+        all other values are ignored, that association matches only that date.
+        <i
+          >Calendar dates should be avoided unless they are the only way to
+          achieve the goal</i
+        >. Calendar dates should be used for saint feast days and the like.
+      </div>
+      <div>
+        <em>Association are evaluated in most-to-least precise order.</em> Seasons,
+        Proper, Weekday, Lectionary Year. The more items that are set, the mpre precise
+        it is. "Lent, 1, Wednesday, A" would match ONLY on Wednesdays during year
+        A during the first week of Lent. but "Leent, Any, Any, Any" would match any
+        day in Lent. The Location Edit screen should give a good indication of the
+        evaluation order.
+      </div>
+      <div>
+        <em>Prayers have a "class" that determines how they are displayed.</em>
+        A prayer with the class "hymn" will display with meter/tune information.
+        A "heartword" will be prefixed with "hear what the spirit is saying.
+        <i
+          >When adding new prayers, it is important to make sure you select the
+          proper class.</i
+        >
+      </div>
+    </Col>
+  </Row>
+
+  <Row>
+    <Col>
+      <h2>Assocation Evaluation Order</h2>
+      <ol>
+        <li>Calendar Date</li>
+        <li>Season, Proper, Weekday, Year</li>
+        <li>Season, Proper, Weekday, Any</li>
+        <li>Season, Proper, Any, Any</li>
+        <li>Season, Any, Weekday, Any</li>
+        <li>Season, Any, Any, Any</li>
+        <li>Any, Any, Any, Any</li>
+      </ol>
+    </Col>
+  </Row>
+
+  <Row>
+    <Col>
+      <h2>Known Bugs/Quirks/Issues</h2>
+      <div>
+        When editing a prayer, edit the body first. If the body is changed after
+        other things (such as the name, or author) the other changes will be
+        reverted when the body is edited.
+      </div>
+      <div>
+        soft-breaks (shift-enter) doesn't work in the body on macOS (untested on
+        windows). Edit the prayer in your word processor, copy-and-paste into
+        the WADO editor
+      </div>
+      <div>There is no way to add a new prayer yet</div>
+      <div>
+        There is no way to edit an association yet, add a new one, delete the
+        old one
+      </div>
+    </Col>
+  </Row>
+</Container>
