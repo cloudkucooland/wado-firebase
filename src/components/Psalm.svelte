8ef62b25b89e3e31390807163c4952bbd899c028
Scot Bontrager <cloudkucooland@gmail.com>
2022-11-11 02:10:51 +0000

    add psalm rubric

--- src/components/Psalm.svelte
+++ src/components/Psalm.svelte
@@ -12,6 +12,9 @@
     <a href="#/edit/{id}">[Edit: {p.name}]</a>
   </div>{/if}
 <h5>{p.name}</h5>
+{#if p.rubric}
+<div class="psalm-rubric">{p.rubric}</div>
+{/if}
 <div class="psalm">{@html p.body}</div>
 {#if p.author}
   <div class="credit">{p.author}</div>
@@ -25,6 +28,12 @@
     font-family: Georgia, serif;
     line-height: 1.5em;
     margin-bottom: 2em;
+  }
+  div.psalm-rubric {
+    font-family: Georgia, serif;
+    line-height: 1.5em;
+    margin-bottom: 2em;
+    font-weight: italic;
   }
   div.credit {
     font-family: Georgia, serif;
