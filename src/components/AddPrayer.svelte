62875c2f8162998b247e2d0b3afa86d3c0cef722
Scot Bontrager <cloudkucooland@gmail.com>
2022-11-20 05:10:29 +0000

    search works

--- src/components/AddPrayer.svelte
+++ src/components/AddPrayer.svelte
@@ -1,5 +1,46 @@
 <script>
-  console.log("add prayer");
+  import { Spinner } from "sveltestrap";
+  import { addDoc, getDocFromServer, collection } from "firebase/firestore";
+  import { db, auth, recordEvent } from "../firebase";
+  import { toasts } from "svelte-toasts";
+  import prayer from "../model/prayer";
+
+  async function addPrayer() {
+    const placeholder = new prayer({
+      Name: "• New Prayer",
+      Body: "Unset",
+      Author: "",
+      Reviewed: false,
+      License: false,
+      "Last Editor": auth.currentUser.displayName,
+      "Last Edited": "never",
+      Class: "prayer",
+    });
+
+    console.log("adding placeholder", placeholder);
+
+    try {
+      const added = await addDoc(
+        collection(db, "prayers"),
+        placeholder.toFirebase()
+      );
+      console.log("added, now refetching");
+      const refetched = await getDocFromServer(added);
+      console.log("refetched", refetched);
+      toasts.success("created");
+      recordEvent("add_prayer", { id: added.id, new: added.id });
+      console.log("loading edit screen", added.id);
+      document.location.assign("#/edit/" + added.id);
+      return added.id;
+    } catch (err) {
+      console.log(err);
+      toasts.error(err.message);
+    }
+  }
 </script>
 
-<h3>Coming soon</h3>
+{#await addPrayer()}
+  <Spinner />
+{:then e}
+  <a href="#/edit/{e}">Edit New Prayer</a>
+{/await}
