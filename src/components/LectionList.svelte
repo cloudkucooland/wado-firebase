eca3019a24a3961dbc12371fde8c2b7ff01390eb
Scot Bontrager <cloudkucooland@gmail.com>
2022-11-23 04:03:49 +0000

    lectionary editor has basic functionality

--- src/components/LectionList.svelte
+++ src/components/LectionList.svelte
@@ -7,21 +7,29 @@
     NavLink,
     ListGroup,
     ListGroupItem,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Input,
+    Button,
   } from "sveltestrap";
   import {
     collection,
     query,
     where,
-    getDocs,
-    getDoc,
+    limit,
     doc,
-    deleteDoc,
-    orderBy,
+    addDoc,
+    setDoc,
   } from "firebase/firestore";
-  import { db, recordEvent, isEditor, screenView } from "../firebase";
-  // import association from "../model/association";
-  // import prayer from "../model/prayer";
-  import season from "../model/season";
+  import {
+    db,
+    recordEvent,
+    isEditor,
+    screenView,
+    getDocsCacheFirst,
+  } from "../firebase";
   import proper from "../model/proper";
   import { onMount } from "svelte";
   import { toasts } from "svelte-toasts";
@@ -30,20 +38,34 @@
   $: year = params.y ? params.y : "A";
   $: lections = new Map();
   let editorPerm = false;
-  let modalId = "exnihilo";
+  $: modalData = {};
 
   async function loadLections(y) {
     const ay = proper.AllYear(y);
+
     for (const [k, v] of ay) {
-      /* try {
-      const q = query(collection(db, "lections", y, "l"));
-      const res = await getDocs(q);
-      for (const a of res.docs) {
-        fromFS.set(a.id, a.data()); // convert to some class
+      try {
+        const q = query(
+          collection(db, "lections", y, "l"),
+          where("season", "==", v.season),
+          where("weekday", "==", v.weekday),
+          where("proper", "==", v.proper),
+          limit(1)
+        );
+        const res = await getDocsCacheFirst(q);
+        for (const a of res.docs) {
+          const n = a.data();
+          v.path = a.ref.path;
+          v.morning = n.morning;
+          v.evening = n.evening;
+          v.morningpsalm = n.morningpsalm;
+          v.eveningpsalm = n.eveningpsalm;
+          ay.set(k, v); // update with data from firestore
+        }
+      } catch (e) {
+        toasts.error(e.message);
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
-    } */
     }
     return ay;
   }
@@ -53,6 +75,59 @@
     screenView("Lection List");
     editorPerm = await isEditor();
   });
+
+  let lectionModalOpen = false;
+  async function toggleLectionModalOpen(e) {
+    screenView("lectionModalOpen");
+    lectionModalOpen = !lectionModalOpen;
+
+    if (lectionModalOpen) {
+      const v = lections.get(e.target.value);
+      modalData = {
+        morning: v.morning,
+        evening: v.evening,
+        morningpsalm: v.morningpsalm,
+        eveningpsalm: v.eveningpsalm,
+        season: v.season,
+        proper: v.proper,
+        weekday: v.weekday,
+        path: v.path,
+        key: e.target.value,
+      };
+    }
+  }
+
+  async function confirmLectionModal() {
+    // console.debug("from modal", modalData);
+    recordEvent("edit_lection", { key: modalData.key });
+    lectionModalOpen = !lectionModalOpen;
+
+    const data = {
+      morning: modalData.morning,
+      morningpsalm: modalData.morningpsalm,
+      evening: modalData.evening,
+      eveningpsalm: modalData.eveningpsalm,
+      season: modalData.season,
+      proper: modalData.proper,
+      weekday: modalData.weekday,
+    };
+    // console.debug("to firestore", data);
+
+    try {
+      if (modalData.path == "" || typeof modalData.path == "undefined") {
+        const added = await addDoc(collection(db, "lections", year, "l"), data);
+        modelData.path = added.ref.path;
+      } else {
+        await setDoc(doc(db, modalData.path), data);
+      }
+    } catch (err) {
+      console.log(err);
+      toasts.error(err.message);
+    }
+    lections.set(modalData.key, modalData);
+    toasts.success("lection set");
+    lections = lections;
+  }
 </script>
 
 <Container>
@@ -64,6 +139,7 @@
 
   <Row class="justify-content-center">
     <Col xs="12" lg="10" xl="8" mx="auto">
+      <strong class="mb-0">Year:</strong>
       <Nav>
         {#each ["A", "B", "C"] as y}
           <NavLink
@@ -85,18 +161,26 @@
             <ListGroupItem>
               <Row class="align-items-center">
                 <Col xs="12" lg="10" xl="8" mx="auto">
-                  <strong class="mb-0">{k}</strong>
+                  {#if editorPerm}
+                    <Button
+                      on:click={toggleLectionModalOpen}
+                      value={k}
+                      size="sm">{k}</Button
+                    >
+                  {:else}
+                    <strong class="mb-0">{k}</strong>
+                  {/if}
                 </Col>
               </Row>
               <Row class="align-items-center">
-                <Col xs="1"><strong>M</strong></Col>
-                <Col xs="2">Morning</Col>
-                <Col xs="1"><strong>E</strong></Col>
-                <Col xs="2">Evening</Col>
-                <Col xs="1"><strong>P</strong></Col>
-                <Col xs="2">Psalm</Col>
-                <Col xs="1"><strong>A</strong></Col>
-                <Col xs="1">Antiphon</Col>
+                <Col xs="1"><strong>M:</strong></Col>
+                <Col xs="2">{v.morning}</Col>
+                <Col xs="1"><strong>E:</strong></Col>
+                <Col xs="2">{v.evening}</Col>
+                <Col xs="1"><strong>MP:</strong></Col>
+                <Col xs="2">{v.morningpsalm}</Col>
+                <Col xs="1"><strong>EP:</strong></Col>
+                <Col xs="1">{v.eveningpsalm}</Col>
               </Row>
             </ListGroupItem>
           {/each}
@@ -105,3 +189,26 @@
     </Col>
   </Row>
 </Container>
+
+<Modal
+  id="lectionModal"
+  isOpen={lectionModalOpen}
+  {toggleLectionModalOpen}
+  size="xl"
+>
+  <ModalHeader {toggleLectionModalOpen}>Edit Lection</ModalHeader>
+  <ModalBody>
+    Morning Psalm: <Input bind:value={modalData.morningpsalm} />
+    Morning: <Input bind:value={modalData.morning} />
+    Evening Psalm: <Input bind:value={modalData.eveningpsalm} />
+    Evening: <Input bind:value={modalData.evening} />
+  </ModalBody>
+  <ModalFooter>
+    <Button color="secondary" size="sm" on:click={toggleLectionModalOpen}>
+      Cancel
+    </Button>
+    <Button color="success" size="sm" on:click={confirmLectionModal}>
+      Confirm
+    </Button>
+  </ModalFooter>
+</Modal>
