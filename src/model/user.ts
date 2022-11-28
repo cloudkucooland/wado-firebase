7556bf632e0f41ab162751dbe7c301ca1758d1d5
Scot Bontrager <cloudkucooland@gmail.com>
2022-11-28 15:38:34 +0000

    maximize dopamine hit

--- src/model/user.ts
+++ src/model/user.ts
@@ -17,6 +17,8 @@ export default class user {
 
     const res = await auth.currentUser.getIdTokenResult();
     console.log(res);
+    this.name = res.Name;
+    // load data from firestore...
   }
 
   public static async UpdateStreak(uid: string) {
@@ -28,48 +30,44 @@ export default class user {
 
     try {
       const loaded = await getDoc(ref);
-      if (loaded.exists()) {
-        const dd = loaded.data();
-        if (!dd.consecutiveDays || !dd.lastDay) {
-          // console.debug("user exists, but no streak data, starting new streak");
-          await setDoc(
-            ref,
-            { consecutiveDays: 1, lastDay: d },
-            { merge: true }
-          );
-          return 1;
-        }
-        if (dd.lastDay == d) {
-          // console.debug("already logged today");
-          return dd.consecutiveDays;
-        }
-
-        const s = d.split("-");
-	const sd = dd.lastDay.split("-");
-        if (s[0] == sd[0] && s[1] == sd[1] && +s[2] == +sd[2] + 1) {
-	  // console.debug("bumping streak!");
-          await setDoc(
-            ref,
-            {
-              consecutiveDays: dd.consecutiveDays + 1,
-              lastDay: d,
-            },
-            { merge: true }
-          );
-          return 1;
-        }
-
-        // console.debug("starting new streak");
-        await setDoc(ref, { consecutiveDays: 1, lastDay: d }, { merge: true });
-        return 1;
-      } else {
-        // console.debug("creating user data, starting new streak");
+      if (!loaded.exists()) {
         await setDoc(ref, { consecutiveDays: 1, lastDay: d });
-        return 1;
+        return "Establishing user data and starting first streak";
       }
+
+      const dd = loaded.data();
+      if (!dd.consecutiveDays || !dd.lastDay) {
+        await setDoc(ref, { consecutiveDays: 1, lastDay: d }, { merge: true });
+        return "Starting first streak";
+      }
+
+      if (dd.lastDay == d) {
+        return "Current streak: " + dd.consecutiveDays + " days";
+      }
+
+      const s = d.split("-");
+      const sd = dd.lastDay.split("-");
+      if (s[0] == sd[0] && s[1] == sd[1] && +s[2] == +sd[2] + 1) {
+        const newStreak = dd.consecutiveDays + 1;
+        await setDoc(
+          ref,
+          {
+            consecutiveDays: newStreak,
+            lastDay: d,
+          },
+          { merge: true }
+        );
+        if (!dd.longestStreak || newStreak > +dd.longestStreak)
+          await setDoc(ref, { longestStreak: newStreak }, { merge: true });
+
+        return "Increasing current streak: " + newStreak + " days";
+      }
+
+      await setDoc(ref, { consecutiveDays: 1, lastDay: d }, { merge: true });
+      return "Starting new streak";
     } catch (err) {
       console.log(err);
-      return 0;
+      return err.message;
     }
   }
 }
