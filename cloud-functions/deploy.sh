9d9d0493d6dbd0277ebe156a052f1e2dcde8df59
Scot Bontrager <cloudkucooland@gmail.com>
2022-12-02 17:28:25 +0000

    limits

--- cloud-functions/deploy.sh
+++ cloud-functions/deploy.sh
@@ -1,1 +1,2 @@
-gcloud functions deploy GetOremus --entry-point GetOremus --runtime go118 --trigger-event "providers/cloud.firestore/eventTypes/document.write" --trigger-resource "projects/osl-dailyoffice/databases/(default)/documents/lections/{year}/l/{id}"
+# When a lection is updated GetOremus (1) pre-caches the data from Oremus (2) links paslms to the lection
+gcloud functions deploy GetOremus --entry-point GetOremus --runtime go118 --trigger-event "providers/cloud.firestore/eventTypes/document.write" --trigger-resource "projects/osl-dailyoffice/databases/(default)/documents/lections/{year}/l/{id}" --max-instances 5  --memory=128
