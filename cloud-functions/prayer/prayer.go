b40aa65c5944b543244b4a06dbe4d0bd090ab744
Scot Bontrager <cloudkucooland@gmail.com>
2022-12-03 17:46:02 -0600

    format

--- cloud-functions/prayer/prayer.go
+++ cloud-functions/prayer/prayer.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"log"
 	"os"
+	"strings"
 	"time"
-    "strings"
 
 	"cloud.google.com/go/firestore"
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	firebase "firebase.google.com/go/v4"
-    secretmanager "cloud.google.com/go/secretmanager/apiv1"
-    "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 
 	"github.com/meilisearch/meilisearch-go"
 )
@@ -24,19 +24,34 @@ type FirestorePrayerEvent struct {
 }
 
 type FirestorePrayerValue struct {
-	CreateTime time.Time   `json:"createTime"`
+	CreateTime time.Time  `json:"createTime"`
 	Fields     PrayerData `json:"fields"`
-	Name       string      `json:"name"`
-	UpdateTime time.Time   `json:"updateTime"`
+	Name       string     `json:"name"`
+	UpdateTime time.Time  `json:"updateTime"`
 }
 
 type PrayerData struct {
-	Body struct { StringValue string `json:"stringValue"` } `json:"Body"`
-	Name struct { StringValue string `json:"stringValue"` } `json:"Name"`
-	Reviewed struct { StringValue string `json:"stringValue"` } `json:"Reviewed"`
-	Class struct { StringValue string `json:"stringValue"` } `json:"Class"`
-	License struct { StringValue string `json:"stringValue"` } `json:"License"`
-	Author struct { StringValue string `json:"stringValue"` } `json:"Author"`
+	Body struct {
+		StringValue string `json:"stringValue"`
+	} `json:"Body"`
+	Name struct {
+		StringValue string `json:"stringValue"`
+	} `json:"Name"`
+	Reviewed struct {
+		StringValue string `json:"stringValue"`
+	} `json:"Reviewed"`
+	Class struct {
+		StringValue string `json:"stringValue"`
+	} `json:"Class"`
+	License struct {
+		StringValue string `json:"stringValue"`
+	} `json:"License"`
+	Author struct {
+		StringValue string `json:"stringValue"`
+	} `json:"Author"`
+    HymnTune struct {
+        StringValue string `json:"stringValue"`
+    } `json:"Hymn Tune"`
 }
 
 var projectID = os.Getenv("GOOGLE_CLOUD_PROJECT")
@@ -58,66 +73,48 @@ func init() {
 		log.Fatalf("app.Firestore: %v", err)
 	}
 
-    if meilihost == "" {
-        meilihost = "https://saint-luke.net:7700"
-    }
+	if meilihost == "" {
+		meilihost = "https://saint-luke.net:7700"
+	}
 
-    secrets, err := secretmanager.NewClient(ctx)
-    if err != nil {
+	secrets, err := secretmanager.NewClient(ctx)
+	if err != nil {
 		log.Fatalf("secretmanager.NewClient: %v", err)
-    }
-    defer secrets.Close()
+	}
+	defer secrets.Close()
 
-    accessRequest := &secretmanagerpb.AccessSecretVersionRequest{
-        Name: "projects/912288843295/secrets/meili/versions/latest",
-    }
+	accessRequest := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: "projects/912288843295/secrets/meili/versions/latest",
+	}
 
-    result, err := secrets.AccessSecretVersion(ctx, accessRequest)
-    if err != nil {
+	result, err := secrets.AccessSecretVersion(ctx, accessRequest)
+	if err != nil {
 		log.Fatalf("secrets.AccessSecretVersion: %v", err)
 	}
 
-    meili = meilisearch.NewClient(meilisearch.ClientConfig{
-        Host: meilihost,
-        APIKey: string(result.Payload.Data),
-    })
+	meili = meilisearch.NewClient(meilisearch.ClientConfig{
+		Host:   meilihost,
+		APIKey: string(result.Payload.Data),
+	})
 }
 
 func UpdateMeiliSearch(ctx context.Context, e FirestorePrayerEvent) error {
-    /* needsupdate := false
-	if e.Value.Fields.Body.StringValue != e.OldValue.Fields.Body.StringValue {
-        needsupdate = true
-    }
-	if e.Value.Fields.Name.StringValue != e.OldValue.Fields.Name.StringValue {
-        needsupdate = true
-    }
-	if e.Value.Fields.Author.StringValue != e.OldValue.Fields.Author.StringValue {
-        needsupdate = true
-    }
-	if e.Value.Fields.Class.StringValue != e.OldValue.Fields.Class.StringValue {
-        needsupdate = true
-    }
-
-    if !needsupdate {
-        log.Println("no need to update")
-        return nil
-    } */
-
-    chunks := strings.Split(e.Value.Name, "/")
-    id := chunks[len(chunks)-1]
+	chunks := strings.Split(e.Value.Name, "/")
+	id := chunks[len(chunks)-1]
 
 	index := meili.Index("prayers")
-    documents := make([]map[string]interface{}, 0)
-    documents = append(documents, map[string]interface{}{
-        "fsid": id,
-        "Author": e.Value.Fields.Author.StringValue,
-        "Body": e.Value.Fields.Body.StringValue,
-        "Name": e.Value.Fields.Name.StringValue,
-        "Class": e.Value.Fields.Class.StringValue,
-        "Reviewed": e.Value.Fields.Reviewed.StringValue == "true",
-        "License": e.Value.Fields.License.StringValue == "true",
-    })
+	documents := make([]map[string]interface{}, 0)
+	documents = append(documents, map[string]interface{}{
+		"fsid":     id,
+		"Author":   e.Value.Fields.Author.StringValue,
+		"Body":     e.Value.Fields.Body.StringValue,
+		"Name":     e.Value.Fields.Name.StringValue,
+		"Class":    e.Value.Fields.Class.StringValue,
+		"Reviewed": e.Value.Fields.Reviewed.StringValue == "true",
+		"License":  e.Value.Fields.License.StringValue == "true",
+		"Hymn Tune":  e.Value.Fields.HymnTune.StringValue,
+	})
 
-    _, err := index.UpdateDocuments(documents, "fsid")
-    return err
+	_, err := index.UpdateDocuments(documents, "fsid")
+	return err
 }
