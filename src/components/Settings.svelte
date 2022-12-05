abc29e2896cf1d88dae81f5b4332bda7805bccf8
Scot Bontrager <cloudkucooland@gmail.com>
2022-12-04 20:08:34 -0600

    set display name

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,6 +1,6 @@
 <script>
   import { showMedia, showEdit, showAlt } from "../model/preferences";
-  import { onMount } from "svelte";
+  import { onMount, getContext } from "svelte";
 
   import {
     Container,
@@ -10,6 +10,7 @@
     ListGroup,
     ListGroupItem,
   } from "sveltestrap";
+  let me = getContext("me");
 
   // does bind not work?
   onMount(() => {
@@ -105,37 +106,32 @@
             </Row>
           </ListGroupItem>
         </ListGroup>
-        <!--
-        <strong class="mb-0">Data Cache</strong>
+        <strong class="mb-0">Profile</strong>
         <ListGroup class="mb-5 shadow">
           <ListGroupItem>
             <Row class="align-items-center">
               <Col>
-                <strong class="mb-0">Offline Data Cache</strong>
-                <p class="text-muted mb-0">Pre-cache the prayer data.</p>
+                <strong class="mb-0">Display Name</strong>
+                <p class="text-muted mb-0">
+                  How your name is presented to others.
+                </p>
               </Col>
               <Col sm="auto">
-                <div class="custom-control custom-switch">
+                <div class="custom-control">
                   <Input
-                    type="checkbox"
                     class="custom-control-input"
-                    id="offline"
+                    id="displayName"
                     on:change={(e) => {
-                      offline.set(e.target.checked);
+                      $me.setDisplayName(e.target.value);
                     }}
+                    value={$me.displayName}
                   />
-                  <span class="custom-control-label">
-                    If you need to use WADO when you don't have data service,
-                    enable this and reload the main page. This will use a lot of
-                    data on the first load, and it will be slow until the entire
-                    load is complete.</span
-                  >
+                  <span class="custom-control-label">Currently Unused.</span>
                 </div>
               </Col>
             </Row>
           </ListGroupItem>
         </ListGroup>
-  -->
       </div></Col
     >
   </Row>
