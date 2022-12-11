f45092b36868419799c3d0b69c350c013836d341
Scot Bontrager <cloudkucooland@gmail.com>
2022-12-10 20:50:44 -0600

    remove unused julian position

--- src/model/season.ts
+++ src/model/season.ts
@@ -1,21 +1,21 @@
 export default class season {
   public name: string;
-  public julianPos: number;
   public churchPos: number;
   public maxProper: number;
   public useWeekdays: boolean;
   public startWeekday: number;
   public maxWeekday: number;
+  public desc: string;
   public color: string;
 
   public constructor(obj: any) {
     this.name = obj.name;
-    this.julianPos = obj.julianPos;
     this.churchPos = obj.churchPos;
     this.maxProper = obj.maxProper;
     this.useWeekdays = obj.useWeekdays;
     this.startWeekday = obj.startWeekday ? obj.startWeekday : 0;
     this.maxWeekday = obj.maxWeekday ? obj.maxWeekday : 0;
+    this.desc = obj.desc ? obj.desc : "";
     this.color = obj.color;
   }
 
@@ -24,7 +24,6 @@ export default class season {
       "advent",
       new season({
         name: "advent",
-        julianPos: 23,
         churchPos: 0,
         maxProper: 4,
         useWeekdays: true,
@@ -35,7 +34,6 @@ export default class season {
       "christmaseve",
       new season({
         name: "christmaseve",
-        julianPos: 24,
         churchPos: 1,
         maxProper: 0,
         useWeekdays: false,
@@ -46,7 +44,6 @@ export default class season {
       "christmasday",
       new season({
         name: "christmasday",
-        julianPos: 25,
         churchPos: 2,
         maxProper: 0,
         useWeekdays: false,
@@ -57,7 +54,6 @@ export default class season {
       "christmas",
       new season({
         name: "christmas",
-        julianPos: 26,
         churchPos: 3,
         maxProper: 12,
         useWeekdays: false,
@@ -68,7 +64,6 @@ export default class season {
       "epiphany",
       new season({
         name: "epiphany",
-        julianPos: 0,
         churchPos: 5,
         maxProper: 0,
         useWeekdays: false,
@@ -79,7 +74,6 @@ export default class season {
       "baptismoflord",
       new season({
         name: "baptismoflord",
-        julianPos: 1,
         churchPos: 6,
         maxProper: 6,
         useWeekdays: false,
@@ -91,7 +85,6 @@ export default class season {
       "afterepiphany",
       new season({
         name: "afterepiphany",
-        julianPos: 2,
         churchPos: 7,
         maxProper: 10,
         maxWeekday: 2,
@@ -103,7 +96,6 @@ export default class season {
       "ashwednesday",
       new season({
         name: "ashwednesday",
-        julianPos: 4,
         churchPos: 9,
         maxProper: 0,
         useWeekdays: false,
@@ -114,7 +106,6 @@ export default class season {
       "lent",
       new season({
         name: "lent",
-        julianPos: 5,
         churchPos: 10,
         maxProper: 6,
         useWeekdays: true,
@@ -127,7 +118,6 @@ export default class season {
       "palmsunday",
       new season({
         name: "palmsunday",
-        julianPos: 6,
         churchPos: 11,
         maxProper: 0,
         useWeekdays: false,
@@ -138,7 +128,6 @@ export default class season {
       "holyweek",
       new season({
         name: "holyweek",
-        julianPos: 7,
         churchPos: 12,
         maxProper: 1,
         useWeekdays: true,
@@ -151,7 +140,6 @@ export default class season {
       "maundythursday",
       new season({
         name: "maundythursday",
-        julianPos: 8,
         churchPos: 13,
         maxProper: 0,
         useWeekdays: false,
@@ -162,7 +150,6 @@ export default class season {
       "goodfriday",
       new season({
         name: "goodfriday",
-        julianPos: 9,
         churchPos: 14,
         maxProper: 0,
         useWeekdays: false,
@@ -173,7 +160,6 @@ export default class season {
       "holysaturday",
       new season({
         name: "holysaturday",
-        julianPos: 10,
         churchPos: 15,
         maxProper: 0,
         useWeekdays: false,
@@ -184,7 +170,6 @@ export default class season {
       "easter",
       new season({
         name: "easter",
-        julianPos: 11,
         churchPos: 16,
         maxProper: 0,
         useWeekdays: false,
@@ -195,7 +180,6 @@ export default class season {
       "greatfifty",
       new season({
         name: "greatfifty",
-        julianPos: 12,
         churhcPos: 17,
         maxProper: 7,
         useWeekdays: true,
@@ -206,7 +190,6 @@ export default class season {
       "ascensioneve",
       new season({
         name: "ascensioneve",
-        julianPos: 13,
         churchPos: 18,
         maxProper: 0,
         useWeekdays: false,
@@ -217,7 +200,6 @@ export default class season {
       "ascension",
       new season({
         name: "ascension",
-        julianPos: 14,
         churchPos: 19,
         maxProper: 0,
         useWeekdays: false,
@@ -228,7 +210,6 @@ export default class season {
       "pentecosteve",
       new season({
         name: "pentecosteve",
-        julianPos: 15,
         churchPos: 20,
         maxProper: 0,
         useWeekdays: false,
@@ -239,7 +220,6 @@ export default class season {
       "pentecost",
       new season({
         name: "pentecost",
-        julianPos: 16,
         churchPos: 21,
         maxProper: 0,
         useWeekdays: false,
@@ -250,7 +230,6 @@ export default class season {
       "afterpentecost",
       new season({
         name: "afterpentecost",
-        julianPos: 17,
         churchPos: 22,
         maxProper: 25,
         useWeekdays: true,
@@ -261,7 +240,6 @@ export default class season {
       "trinity",
       new season({
         name: "trinity",
-        julianPos: 18,
         churchPos: 23,
         maxProper: 0,
         useWeekdays: false,
@@ -272,7 +250,6 @@ export default class season {
       "christking",
       new season({
         name: "christking",
-        julianPos: 20,
         churchPos: 24,
         maxProper: 0,
         useWeekdays: false,
@@ -283,7 +260,6 @@ export default class season {
       "Any",
       new season({
         name: "Any",
-        julianPos: 99,
         churchPos: 4,
         maxProper: 0,
         useWeekdays: true,
