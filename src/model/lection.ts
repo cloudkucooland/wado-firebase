4a6e666347984888df26f28b6bbac0650cf83756
Scot Bontrager <cloudkucooland@gmail.com>
2022-12-18 19:57:43 -0600

    lections completely changed

--- src/model/lection.ts
+++ src/model/lection.ts
@@ -1,7 +1,25 @@
-import prayer from "./prayer";
+export default class lection {
+  morning?: string;
+  evening?: string;
+  morningpsalm?: string;
+  eveningpsalm?: string;
+  morningtitle?: string;
+  eveningtitle?: string;
+  _morning?: string;
+  _evening?: string;
+  _morningpsalmref?: string;
+  _eveningpsalmref?: string;
 
-export default class lection extends prayer {
   public constructor(obj: any) {
-    super(obj);
+    if (obj.morning) this.morning = obj.morning;
+    if (obj.evening) this.evening = obj.evening;
+    if (obj.morningpsalm) this.morningpsalm = obj.morningpsalm;
+    if (obj.eveningpsalm) this.eveningpsalm = obj.eveningpsalm;
+    if (obj.morningtitle) this.morningtitle = obj.morningpsalm;
+    if (obj.eveningtitle) this.eveningtitle = obj.eveningtitle;
+    if (obj._morning) this._morning = obj._morning;
+    if (obj._evening) this._evening = obj._evening;
+    if (obj._morningpsalmref) this._morningpsalmref = obj._morningpsalmref;
+    if (obj._eveningpsalmref) this._eveningpsalmref = obj._eveningpsalmref;
   }
 }
