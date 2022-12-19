97ccb62bad7610b42d8cbcf73771355cfe1883c9
Scot Bontrager <cloudkucooland@gmail.com>
2022-12-18 19:58:06 -0600

    lection is no longer a prayerClass

--- src/model/prayerClasses.ts
+++ src/model/prayerClasses.ts
@@ -1,7 +1,6 @@
 import prayer from "./prayer";
 import psalm from "./psalm";
 import hymn from "./hymn";
-import lection from "./lection";
 import heartword from "./heartword";
 import antiphon from "./antiphon";
 
@@ -9,7 +8,6 @@ export const classes = new Map([
   ["prayer", prayer],
   ["psalm", psalm],
   ["hymn", hymn],
-  ["lection", lection],
   ["heartword", heartword],
   ["antiphon", antiphon],
 ]);
