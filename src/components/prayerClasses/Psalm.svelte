fc43f63ece67514125e00a2c810607319f0e528c
Scot Bontrager <cloudkucooland@gmail.com>
2023-01-12 19:27:51 -0600

    antiphons on psalms

--- src/components/prayerClasses/Psalm.svelte
+++ src/components/prayerClasses/Psalm.svelte
@@ -7,6 +7,9 @@
   import { getContext } from "svelte";
   import { Icon } from "sveltestrap";
   import { push } from "svelte-spa-router";
+  import { toasts } from "svelte-toasts";
+  import Antiphon from "./Antiphon.svelte";
+  import { getDoc } from "firebase/firestore";
 
   export let data: prayerFromFirestore;
   export let id: string;
@@ -14,6 +17,20 @@
   const p = new psalm(data);
   p.id = id;
   const qe: Readable<any> = getContext("qe");
+  let antiphon: prayerFromFirestore;
+  let antID: string;
+
+  if (p.antiphon) {
+    try {
+      getDoc(p.antiphon).then((snap) => {
+        antID = snap.id;
+        antiphon = snap.data() as prayerFromFirestore;
+      });
+    } catch (err) {
+      console.log(err);
+      toasts.error(err.message);
+    }
+  }
 </script>
 
 <h5>
@@ -34,8 +51,14 @@
 {#if p.rubric}
   <div class="psalm-rubric">{p.rubric}</div>
 {/if}
+{#if antiphon}
+  <Antiphon data={antiphon} id={antID} />
+{/if}
 <div class="psalm">{@html p.body}</div>
 {#if p.author}
   <div class="psalm-credit">{p.author}</div>
 {/if}
 <Media mediaUrl={p.media} />
+{#if antiphon}
+  <Antiphon data={antiphon} id={antID} />
+{/if}
