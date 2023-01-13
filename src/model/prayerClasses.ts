2bc3f59e721100760bc524ebabb17bd097c0bf12
Scot Bontrager <cloudkucooland@gmail.com>
2023-01-12 19:30:40 -0600

    types

--- src/model/prayerClasses.ts
+++ src/model/prayerClasses.ts
@@ -4,7 +4,19 @@ import hymn from "./hymn";
 import heartword from "./heartword";
 import antiphon from "./antiphon";
 
-export const classes = new Map([
+export interface prayerClass {
+  name: string;
+  body: string;
+  reviewed: boolean;
+  license: boolean;
+  lastEditor: string;
+  lastEdited: string;
+  author: string;
+  media: string;
+  class: string;
+}
+
+export const classes: Map<string, prayerClass> = new Map([
   ["prayer", prayer],
   ["psalm", psalm],
   ["hymn", hymn],
@@ -12,7 +24,7 @@ export const classes = new Map([
   ["antiphon", antiphon],
 ]);
 
-export function getClass(className: string) {
+export function getClass(className: string): prayerClass {
   if (!classes.has(className)) {
     console.log("invalid class", className);
     className = "prayer";
