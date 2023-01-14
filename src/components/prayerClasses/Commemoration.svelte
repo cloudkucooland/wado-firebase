2cd91a3d8722825d5c5401a730ecacf69d2eddd6
Scot Bontrager <cloudkucooland@gmail.com>
2023-01-14 13:23:16 -0600

    tweak

--- src/components/prayerClasses/Commemoration.svelte
+++ src/components/prayerClasses/Commemoration.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { showEdit } from "../../model/preferences";
   import Media from "../Media.svelte";
-  import prayer from "../../model/prayer";
+  import commemoration from "../../model/commemoration";
   import type { prayerFromFirestore } from "../../model/types";
   import type { Readable } from "svelte/store";
   import { getContext } from "svelte";
@@ -11,8 +11,8 @@
   export let data: prayerFromFirestore;
   export let id: string;
 
-  const p = new prayer(data);
-  p.id = id;
+  const c = new commemoration(data);
+  c.id = id;
   const qe: Readable<any> = getContext("qe");
 </script>
 
@@ -24,13 +24,13 @@
     >
       <Icon name="pencil" />
     </button>
-    <button on:click={$qe(p)}>
+    <button on:click={$qe(c)}>
       <Icon name="pencil-square" />
     </button>
   </div>{/if}
-<h6>Commemoration</h6>
-<div>{@html p.body}</div>
-<h6>Collect of Commemoration</h6>
-<div>{@html p.collect}</div>
-{#if p.author}<div class="prayer-credit">{p.author}</div>{/if}
-<Media mediaUrl={p.media} />
+<h6>Commemoration Reading</h6>
+<div>{@html c.body}</div>
+{#if c.collect}<h6>Commemoration Collect</h6>
+  <div>{@html c.collect}</div>{/if}
+{#if c.author}<div class="prayer-credit">{c.author}</div>{/if}
+<Media mediaUrl={c.media} />
