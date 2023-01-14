e3fa964d12c27432d86142f6d533a7ffdec60be2
Scot Bontrager <cloudkucooland@gmail.com>
2023-01-14 13:23:30 -0600

    what I'm testing now

--- src/components/offices/TestOffice.svelte
+++ src/components/offices/TestOffice.svelte
@@ -2,22 +2,22 @@
   import Location from "../Location.svelte";
   import Silence from "../Silence.svelte";
   import Rubric from "../Rubric.svelte";
+  import PsalmLection from "../PsalmLection.svelte";
 </script>
 
-<Rubric>This is a rubric</Rubric>
+<h3>Commemoration test</h3>
+<Location name="VESPER-COMMEMORATION" />
+<Location name="VESPER-PARDON" />
+
+<!-- <Rubric>This is a rubric</Rubric>
 <Silence />
-<Silence>Silence with content</Silence>
+<Silence>Silence with content</Silence> -->
 
-<h3>Lectionary w/ alternatives</h3>
-<Location name="LAUDS-LECTIONARY" maxAlt={5} />
+<!-- <h3>Multiples:</h3>
+<Location name="LAUDS-COLLECT1" max={5} /> -->
 
-<h3>KYRIE w/ alternates</h3>
-<Location name="KYRIE" maxAlt={5} />
-
-<h3>The Lord's Prayer w/ alternatives</h3>
-<Location name="GENERAL-LORDS" maxAlt={5} />
+<!-- <h3>KYRIE w/ alternates</h3>
+<Location name="KYRIE" maxAlt={5} /> -->
 
 <h3>Antiphon test</h3>
-<Location name="LAUDS-PSALTER-ANTIPHON" />
-<Location name="LAUDS-PSALM" />
-<Location name="LAUDS-PSALTER-ANTIPHON" />
+<PsalmLection office="COMPLINE" />
