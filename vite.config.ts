d6ebf624257b329fd34660ff0685b5acde390043
Scot Bontrager <cloudkucooland@gmail.com>
2025-10-06 22:14:54 +0000

    more splits, this actually makes the main module smaller

--- vite.config.ts
+++ vite.config.ts
@@ -32,71 +32,14 @@ export default defineConfig({
 		rollupOptions: {
 			output: {
 				manualChunks: {
-					// manualChunks -- texteditor and core are probably enough, and meilisearch
-					// things which are not used by the normal end-user
-					tiptap: [
-						'@flowbite-svelte-plugins/texteditor',
-						'@tiptap/core',
-						'@tiptap/extension-blockquote',
-						'@tiptap/extension-bold',
-						'@tiptap/extension-bullet-list',
-						'@tiptap/extension-character-count',
-						'@tiptap/extension-code',
-						'@tiptap/extension-code-block',
-						'@tiptap/extension-code-block-lowlight',
-						'@tiptap/extension-color',
-						'@tiptap/extension-details',
-						'@tiptap/extension-document',
-						'@tiptap/extension-dropcursor',
-						'@tiptap/extension-emoji',
-						'@tiptap/extension-file-handler',
-						'@tiptap/extension-focus',
-						'@tiptap/extension-font-family',
-						'@tiptap/extension-gapcursor',
-						'@tiptap/extension-hard-break',
-						'@tiptap/extension-heading',
-						'@tiptap/extension-highlight',
-						'@tiptap/extension-history',
-						'@tiptap/extension-horizontal-rule',
-						'@tiptap/extension-image',
-						'@tiptap/extension-invisible-characters',
-						'@tiptap/extension-italic',
-						'@tiptap/extension-link',
-						'@tiptap/extension-list',
-						'@tiptap/extension-list-item',
-						'@tiptap/extension-list-keymap',
-						'@tiptap/extension-mathematics',
-						'@tiptap/extension-mention',
-						'@tiptap/extension-node-range',
-						'@tiptap/extension-ordered-list',
-						'@tiptap/extension-paragraph',
-						'@tiptap/extension-placeholder',
-						'@tiptap/extension-strike',
-						'@tiptap/extension-subscript',
-						'@tiptap/extension-superscript',
-						'@tiptap/extension-table',
-						'@tiptap/extension-table-cell',
-						'@tiptap/extension-table-header',
-						'@tiptap/extension-table-of-contents',
-						'@tiptap/extension-table-row',
-						'@tiptap/extension-task-item',
-						'@tiptap/extension-task-list',
-						'@tiptap/extension-text',
-						'@tiptap/extension-text-align',
-						'@tiptap/extension-text-style',
-						'@tiptap/extension-typography',
-						'@tiptap/extension-underline',
-						'@tiptap/extension-youtube',
-						'@tiptap/extensions',
-						// '@tiptap/pm',
-						'@tiptap/starter-kit',
-						'@tiptap/suggestion',
-						'meilisearch'
-					]
+					tiptap: ['@flowbite-svelte-plugins/texteditor', '@tiptap/core'],
+					meili: ['meilisearch'],
+					icons: ['flowbite-svelte-icons']
 				}
 			}
 		}
 	},
+	esbuild: { legalComments: 'none' },
 	rollupdedupe: ['svelte']
 });
 
