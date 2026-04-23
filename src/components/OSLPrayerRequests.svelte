4b0ae8fb57cf56e6a4bf1104ebc39f06bc98a526
Scot Bontrager <cloudkucooland@gmail.com>
2026-04-23 01:00:33 +0000

    add

---
+++ src/components/OSLPrayerRequests.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { Spinner } from 'flowbite-svelte';
+	import Subheading from './Subheading.svelte';
+	import { getContext } from 'svelte';
+
+	const dateContext = getContext<{ value: string }>('officeDate');
+	let currentOffsetDate = $derived(dateContext.value);
+
+	async function loaddata(od: string): Promise<Array<any>> {
+		const d = od.value.split('-');
+		const month = d[1];
+		const day = d[2];
+
+		const cred: RequestCredentials = 'include';
+		const mode: RequestMode = 'cors';
+		const redirect: RequestRedirect = 'manual';
+		const rp: ReferrerPolicy = 'origin';
+
+		const request = {
+			method: 'GET',
+			mode: mode,
+			credential: cred,
+			redirect: redirect,
+			referrerPolicy: rp
+		};
+
+		const response = await fetch(
+			`https://saint-luke.net:8443/api/v1/prayers`,
+			request
+		);
+		const payload = await response.json();
+		if (response.status != 200) {
+			console.log('server returned ', response.status);
+			throw new Error(payload.error);
+		}
+		return payload;
+	}
+</script>
+
+{#await loaddata(dateContext)}
+	<Spinner />
+{:then data}
+	{#if data.length > 0}
+		<Subheading>Prayer Requests</Subheading>
+		{#each data as d}
+			<div>{d.Content} ({d.OSLName})</div>
+		{/each}
+	{/if}
+{:catch error}
+	<h5>Unable to load: {error.message}</h5>
+{/await}
